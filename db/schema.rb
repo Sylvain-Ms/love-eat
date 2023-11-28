cba1bf4217996c9d4b351a163c07da078213c11d
jessy toison <jessytoison@gmail.com>
2023-11-28 16:05:22 +0100

    fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_27_163509) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_28_145716) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -104,10 +104,11 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_27_163509) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.integer "age"
     t.text "description"
     t.string "price"
-    t.integer "dietdistance"
+    t.datetime "birthdate"
+    t.string "diet"
+    t.integer "distance"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
