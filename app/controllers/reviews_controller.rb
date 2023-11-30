004694d7349db77de426e24f1454eb9077cf0612
sylvain mesnage <sylvain368@hotmail.fr>
2023-11-30 16:13:02 +0100

    review_create

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,10 +2,26 @@ class ReviewsController < ApplicationController
 
   def new
     @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
   def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    @review.user = current_user
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
 
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating,:content)
+  end
 end
