19141c2be2fd9ddcad6a6377d55c963fb8645429
gaetan martinez <gaetan.martinez81100@gmail.com>
2023-11-30 15:27:58 +0100

    fetch API Tripadvisor

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,13 @@ class Restaurant < ApplicationRecord
   has_many :users, through: :suggestions
   has_many :reviews, dependent: :destroy
   validates :name, :address, :phone_number, :category, :menu, presence: true
+
+  def self.save_csv(restaurants)
+    CSV.open("#{root_path}/db/restaurants.csv", "a") do |csv|
+      csv << %w[id name price]
+      restaurants.each do |restaurant|
+        csv << [restaurant.name, restaurant.address, restaurant.phone_number, restaurant.category, restaurant.menu, restaurant.price]
+      end
+    end
+  end
 end
