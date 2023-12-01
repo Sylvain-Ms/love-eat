38e2599205e311ef5a266fe69827b3a23eef3cc9
gaetan martinez <gaetan.martinez81100@gmail.com>
2023-12-01 17:07:06 +0100

    match_list

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,14 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :user_liked, class_name: "User", foreign_key: 'user_liked_id'
+
+  scope :match, -> (user) do
+    find_by_sql("SELECT l.id, l.user_id, l.user_liked_id, ul.user_id, ul.user_liked_id
+      FROM likes l JOIN likes ul ON l.user_liked_id = ul.user_id
+      WHERE l.user_id = ul.user_liked_id AND l.user_id = #{user.id}")
+  end
+
+  def match?(user_liked)
+    self.user_liked == user_liked && user_liked.likes.find(user_liked: current_user)
+  end
 end
