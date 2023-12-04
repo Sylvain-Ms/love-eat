41f8537f7457fae1016f76fd1e72cb0d42365688
sylvain mesnage <sylvain368@hotmail.fr>
2023-12-04 15:30:32 +0100

    stimulus_food_card

--- app/javascript/controllers/food_card_controller.js
+++ app/javascript/controllers/food_card_controller.js
@@ -5,8 +5,31 @@ export default class extends Controller {
   static targets = ["card"]
 
   connect() {
+    this.list = []
   }
+
   select(event) {
-    this.cardTarget.classList.toggle('card_selected')
+    event.currentTarget.classList.toggle('card_selected')
+    // si l'id est deja dans la list
+    if (this.list.includes(event.currentTarget.id)) {
+      // trouver l'index de l'id
+      const index = this.list.indexOf(event.currentTarget.id)
+      // enlever l'id de la list
+      this.list.splice(index, 1)
+    }
+    // sinon{}
+    else {
+      // le rajouter
+      this.list.push(event.currentTarget.id)
+    }
+    console.log(this.list);
+  }
+
+  submit(e) {
+    console.log(e)
+  }
+
+  prolonge(e) {
+    e.currentTarget.previousElementSibling.click()
   }
 }
