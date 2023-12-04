7e0a45ef164cf200d737b14463a7a45f7f3f750c
jessy toison <jessytoison@gmail.com>
2023-12-04 11:10:43 +0100

    dislike button suppress element card

--- app/javascript/controllers/swipe_controller.js
+++ app/javascript/controllers/swipe_controller.js
@@ -3,12 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="swipe"
 export default class extends Controller {
   static targets = ["refuseButton", "likeButton"]
-  static values = /* https://stimulus.hotwired.dev/reference/values */
+  static values = {
+    userLikedId: Number
+  }
   connect() {
-    console.log(this.refuseButtonTarget, this.likeButtonTarget)
+    console.log(this.userLikedIdValue)
   }
 
-  swipe(event){
+  swipe(event) {
     console.log(event)
     /*  */
   }
@@ -17,12 +19,16 @@ export default class extends Controller {
     console.log(event)
     /* notice: "You have liked this user 💛" */
     /* mettre le user_liked dans la DB */
-    /* ne plus montrer le user_like dans User.all */
+      /* fetch(``)
+        .then(response => response.json())
+        .then((data) => {
+      console.log(data) */
+    /* ne plus montrer le user_like dans User.all ? */
+          /* this.element.remove() */
   }
 
-  refuse(event){
+  refuse(event) {
     console.log(event)
-    /* mettre le user_refused dans la DB */
-    /* ne plus montrer le user_like dans User.all */
+    this.element.remove()
   }
 }
