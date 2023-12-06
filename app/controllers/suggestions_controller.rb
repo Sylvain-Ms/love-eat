9e6b2fd27380d3aa703e24c4d4b62ffc82a00bf2
Maxime Rollinde de Beaumont <maximerdb@hotmail.fr>
2023-12-06 16:08:30 +0100

    create controller suggestions

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -13,11 +13,26 @@ class SuggestionsController < ApplicationController
     @suggestion.user = current_user
     @restaurant = Restaurant.find(params[:restaurant_id])
     @suggestion.restaurant = @restaurant
+    @suggestion.user_liked_id = user_liked_id
 
     if @suggestion.save!
       redirect_to root_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @suggestion = Suggestion.find(params[:id])
+  end
+
+  def update
+    @suggestion = Suggestion.find(params[:id])
+
+    if @suggestion.update(suggestion_params)
+      redirect_to @suggestion
+    else
+      render :edit
     end
   end
 
