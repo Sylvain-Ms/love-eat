0a826ddea859a32b0fbaffd1831e32919cd5a97a
sylvain mesnage <sylvain368@hotmail.fr>
2023-12-06 11:36:23 +0100

    suggestion_cards
    Signed-off-by: sylvain mesnage <sylvain368@hotmail.fr>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_05_111918) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_06_085935) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -99,6 +99,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_05_111918) do
     t.string "hour"
     t.string "url"
     t.text "descripton"
+    t.string "image_url"
   end
 
   create_table "reviews", force: :cascade do |t|
