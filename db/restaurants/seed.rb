79f846ca8876475a2268f13c4e8ba8ec5848c00d
jessy toison <jessytoison@gmail.com>
2023-12-07 11:13:36 +0100

    fix seed for everyone

--- db/restaurants/seed.rb
+++ db/restaurants/seed.rb
@@ -1,11 +1,12 @@
 require "json"
 
 def create_restaurants
-  for i in [1, 2, 3]
+  for i in [1, 2, 3, 4]
     file = File.read("db/restaurants/toulouse#{i}.json")
     data_hash = JSON.parse(file)
     for restaurant in data_hash
-      Restaurant.create(name: restaurant['name'], address: restaurant['parentGeoName'], category: restaurant['establishmentTypeAndCuisineTags'][0], menu: restaurant['menuUrl'], price: restaurant['priceTag'], image_url: restaurant['heroImgUrl'])
+      Restaurant.create!(name: restaurant['name'], address: restaurant['parentGeoName'], category: restaurant['establishmentTypeAndCuisineTags'][0], menu: restaurant['menuUrl'], price: restaurant['priceTag'], image_url: restaurant['heroImgUrl'])
+      p Restaurant.last.name
     end
   end
 end
